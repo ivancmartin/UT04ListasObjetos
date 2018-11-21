a3e06b18561151dc280748cfcccc74a91b3ad5b6
ivancmartin <ivan.cmartin94@gmail.com>
2018-11-21 18:38:34 +0100

    pruebas con objetos: realizando pruebas

--- conFunciones/js/listas.js
+++ conFunciones/js/listas.js
@@ -1,43 +1,86 @@
 "use strict";
 
-var arrayP = [];
-
-/* cosntructor de person */
-var Person = {
-    pname : "default-name",
-    psurname : "default-surname",
-    pfullname : function(){ return this.pname + " " + this.psurname} 
+/*Objetos personas*/
+function Person(name,surname){
+    this.name = name || "default-name";
+    this.surname = surname || "default-surname";
+    this.fullname = this.name + " " + this.surname; 
 }
 
-Object.defineProperty(Person,'pfullname',{
-    writable: false,
-    enumerable: false
-});
+function Person1(name,surname){
+    this.name = name || "default-name";
+    this.surname = surname || "default-surname";
+    this.fullname = this.name + " " + this.surname; 
+}
 
-//la variable objeto
-var PersonConst = Object.create(Person);
+function Lista (){
+    var array = []; 
+    const size = 5;
 
-PersonConst.pname = "ivan";
-PersonConst.psurname = "cañizares";
+    this.isEmpty = function(){
+        return (array.length === 0);
+    }
 
-arrayP.push(PersonConst);
+    this.isFull = function(){
+        return (array.length > size  ? true : false );
+    }
 
-var PersonConst = Object.create(Person);
-PersonConst.pname = "beatriz";
-PersonConst.psurname = "martin";
+    this.listSize = function (){
+        return array.length;
+    }
 
-arrayP.push(PersonConst);
+    this.addPerson = function(objPerson){
+        
+        
+        //si el elemento NO es un número
+        if(objPerson instanceof Person){
+            throw "Eso no es un número.";
+        }
+        //si la lista está llena
+        if(!isFull()){
+            array.push(objPerson);
+        }else{
+            throw "esto está lleno!";
+        }
+        return size(list);
 
-arrayP.forEach(function(valor,indice,arrayp){
-    console.log(valor.pname +  " " + indice);
+    }
     
-})
+    this.getArray = function(){
+        for (var i = 0; i < array.length; i++) {
+            console.log(array[i].name);
+        }
+    }
+    
+    
+}
 
-
-
-console.log(PersonConst.pname + " " + PersonConst.psurname );
 
 //funciones de testeo
 function testListObj(){
-    	
+
+    var per1 = new Person("ivan","cañizares");
+    var per2 = new Person("beatriz","perez");
+    var per3 = new Person();
+    var per4 = new Person("ruben","martin");
+    var per5 = new Person("maria","caballero");
+    var per2 = new Person1("extra","extra");
+
+   
+    var list = new Lista();
+    list.addPerson(per1);
+    list.addPerson(per1);
+    list.addPerson(per1);
+    list.addPerson(per1);
+    list.addPerson(per1);
+    list.addPerson(per1);
+
+    console.log(list.listSize());
+
+
+    console.log(list.isEmpty());
+    console.log(list.isFull());
+    var miArray =  list.getArray();
+	
 }
+window.onload = (testListObj);
