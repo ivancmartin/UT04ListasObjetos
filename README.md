8b1950d3b2c73023b8b66ab1d7c2c6b83c4a436c
unknown <ivan.cmartin94@gmail.com>
2018-11-25 16:09:24 +0100

    version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # UT04ListasObjetos
 UT04 Práctica Listas y Listas Ordenadas con objetos
+
+#0.1.0
+Objetos principales: 
+    - Person.
+        + name
+        + surname
+        + fullName() 
+    - Person1 (utilizado para realizar pruebas con instance of).
+        + name
+        + surname
+        + fullName() 
+    - Lista (un array interno para almacenar objetos de tipo Person).
+        + isFull: devuelve si esta lleno o no el array.
+        + isEmpty: devuelve si esta vacío o no el array.
+        + listSize: devuelve tamaño de nuestro array según se va llenando.
+        + addPerson: añade un objeto Person Al final del array(interno).
+        + addPersonAt: añade un objeto Person Al final del array(interno) en la posición indicada.
+        + create: crea el array con un tamaño fijo.
+        + getPerson: devuelve el objeto Person indicado.
+        + clear: vacía el array.
+        + firstElement: devuelve el 1º objeto del array.
+        + lastElement devuelve el último elemento del array.
+        + remove: elimina el objeto del indice seleccionado.
+        + removeElement: borra un objeto concreto del array.
+        + set: sustituye un objeto por otro indicado.
+        + toStringArray: muestra por pantalla los valores de los objetos almacenados en el array.
+    - myError (objeto para definir formatos propios de errores).
+        + noneInstanceOf(myError) : el objeto no es una isntancia de Person:
+        + notFound(myError) : elemento no encontrado.
+        + fullOf(myError): array lleno.
+        + emptyOf(myError): array vacío.
+        + outOfIndex(myError): indice fuera de rango.
+        + incorrectIndex(myError): indice introducido incorrecto.
+    - testListObj: testeo de las funciones del objeto Lista.
+
--- conFunciones/js/listas.js
+++ conFunciones/js/listas.js
@@ -1,19 +1,28 @@
 "use strict";
 
-/*Objetos personas*/
+/*Objetos Person, Person1*/
 function Person(name,surname){
     this.name = name || "default-name";
     this.surname = surname || "default-surname";
-    //this.fullname = name + " " + surname; <-- corregir 
+    this.fullname = function(){
+        return name + " " + surname;
+    }
 }
 
-/* usado para comprobar las instancias(funciones de test) */
+/*Objeto person1: usado para comprobar las instancias(funciones de test) */
 function Person1(name,surname){
     this.name = name || "default-name";
     this.surname = surname || "default-surname";
-    //this.fullname = this.name + " " + this.surname; <-- corregir
+    this.fullname = function(){
+        return name + " " + surname;
+    }
 }
 
+
+Object.defineProperty
+/* fin objetos Person, Person1 */
+
+/* objeto Lista */
 function Lista (){
 
     var array = []; 
@@ -37,16 +46,17 @@ function Lista (){
     //añade un objeto de tipo Persona fuera
     this.addPerson = function(objPerson){
 
-        //si el elemento NO es un número
+        //si el elemento NO es un objeto de tipo Person
         if(!(objPerson instanceof Person)){
-            throw "Eso no es un objeto persona.";
+           // throw "Eso no es un objeto persona.";
+           throw new noneInstanceOf();
         }
         
         //si la lista está llena
         if(!this.isFull()){
             array.push(objPerson);
         }else{
-            throw "esto está lleno!";
+            throw new fullOf();
         }
 
         //devolvemos 
@@ -56,43 +66,62 @@ function Lista (){
     //Añade a la lista un elemento de tipo Number y devuelve el tamaño actual del array
     this.addPersonAt = function(objPerson,index){
         
-        //Eso no es un Objeto persona
+        index = index || 0;
+
+        if(isNaN(index)){
+            throw new incorrectIndex(index);
+        }
+
+        //si el elemento NO es un objeto de tipo Person
         if(!(objPerson instanceof Person)){
-            throw "Eso no es un Objeto persona";
+            // throw "Eso no es un objeto persona.";
+            throw new noneInstanceOf();
         }
+
         //control sobre el indice: si esta fuera de rango 
-        if((index < 0 || index > this.size)){
-            throw "Eso no es un indice válido (max " + tam + " )...";
+        var range = this.listSize(array)-1;
+        if((index < 0 || index > range)){
+            throw new outOfIndex(range);
         }
+
         //si la lista NO está llena
         if(!this.isFull()){
             
             //si está vacía se añade al principio
             if(this.isEmpty()){
-               
                 array.push(objPerson);
             }else if( (array[index]) instanceof Person ){
-                
                 array.splice(index, 0, objPerson);        
             }
+
         }else{
-            throw "esto está lleno!";
+            throw new fullOf();
         }
         return this.listSize(array);
     }
 
     // devuelve el elemento encontrado en el indice selecionado dentro del array
     this.getPerson = function(index){
-        if(array.includes(array[index])){
-            return array[index];
-        }else{
-            throw "El elemento no existe";
+        index = index || 0; 
+
+        if(isNaN(index)){
+            throw new incorrectIndex(index);
         }
+
+        //control sobre el indice: si esta fuera de rango 
+        var range = this.listSize(array)-1;
+        if((index < 0 || index > range)){
+            throw new outOfIndex(range);
+        }else{
+            return array[index];
+        }
+        
     }
 
-    //vacia una lista de elementos: pasa los valores a NaN
+    //vacia una lista de elementos
     this.clear = function(){
-        return array.splice(array,size);
+       array.splice(array,this.listSize(array));
+       return this.listSize(array);
     }
 
     //devuelve el 1º elemento de un array
@@ -101,7 +130,7 @@ function Lista (){
         if(!this.isEmpty(array)){
             last = array[0];
         }else{
-            throw "La lista está vacía";
+            throw new emptyOf();
         }
         return last;
     }
@@ -112,7 +141,7 @@ function Lista (){
         if(!this.isEmpty(array)){
             last = array[this.listSize() - 1];
         }else{
-            throw "La lista está vacía";
+            throw new emptyOf();
         }
         return last;
     }
@@ -122,14 +151,18 @@ function Lista (){
 
         index = index || 0;
 
+        if(isNaN(index)){
+            throw new incorrectIndex(index);
+        }
+
         if(this.isEmpty(array)){    
-            throw "La lista está vacía";
+            throw new emptyOf();
         }
 
         var range = this.listSize(array)-1;
         //console.log(range);
         if(index > range || index < 0){    
-            throw "El indice es incorrecto: debe estar entre el 0 y " + range;
+            throw new outOfIndex(range);
         }
         //guardamos el elemento que vamos a borrar
         var deleted = array[index];
@@ -140,19 +173,20 @@ function Lista (){
     }
 
     //elimina el indice del array
-    this.removeElement = function(elem){
+    this.removeElement = function(objPerson){
 
         if(this.isEmpty(array)){    
-            throw "La lista está vacía";
+            throw new emptyOf();
         }
 
-        console.log("valor del elemento a borrar " + array.includes(elem));
+        //console.log("valor del elemento a borrar " + array.includes(objPerson));
         
-        if(!array.includes(elem)){
-            throw "el valor no esta en la lista";
+        if(!array.includes(objPerson)){
+            throw new notFound();
         }
+        
         //recogemos el valor del indice de la 1º coincidencia
-        var index = array.indexOf(elem);
+        var index = array.indexOf(objPerson);
         //guardamos el elemento que vamos a borrar
         var deleted = array[index];
         //borramos el elemento
@@ -162,21 +196,32 @@ function Lista (){
     }
     
     //sustituye el valor del indice, devuelve el valor sustituido
-    this.set = function(elem,index){
+    this.set = function(objPerson,index){
         
-        if(isEmpty(list)){    
-            throw "La lista está vacía";
-        }
-        
-        var range = size(list)-1;
-        
-        if(index > range || index < 0){    
-            throw "El indice es incorrecto: debe estar entre el 0 y " + range;
+        index = index || 0;
+
+        if(isNaN(index)){
+            throw new incorrectIndex(index);
         }
 
-        var replace = list[index]
+        if(this.isEmpty(array)){    
+            throw new emptyOf();
+        }
+
+        //si el elemento NO es un número
+        if(!(objPerson instanceof Person)){
+            throw new noneInstanceOf();
+        }
+
+        var range = this.listSize(array)-1;
         
-        list.splice(index,1,elem);
+        if(index > range || index < 0){    
+            throw new outOfIndex(range);
+        }
+
+        var replace = array[index]
+        
+        array.splice(index,1,objPerson);
         
         return replace;
     }
@@ -184,36 +229,137 @@ function Lista (){
     //muestra por pantalla los valores del array
     this.toStringArray = function(){
         for (var i = 0; i < this.listSize(); i++) {
-            console.log(array[i].name);
+            console.log(array[i].fullname());
         }
     }
+}
+/* fin objeto lista*/
 
-        
 
-    }
+/* objeto myError */
+function myError (){}; //objeto vacío
+myError.prototype = new Error(); //heredamos del objeto error
+myError.prototype.constructor = myError(); // creamos un constructor para myError
+myError.prototype.toString = function(){ //sobreescribimos el metodo toString para nuestro objeto
+    return  this.name  + " : " + this.message;
+};
 
+// tipo de error personalizado: el objeto no es una isntancia de Person
+function noneInstanceOf(){ 
+    this.name = "noneInstanceOf";
+    this.message = "Este objeto no es una instancia de Person";
+}
+
+noneInstanceOf.prototype = new myError(); //hereda de myError
+noneInstanceOf.prototype.constructor = noneInstanceOf; // creamos un constructor para este tipo de error
+
+// tipo de error personalizado: elemento no encontrado
+function notFound(){ 
+    this.name = "notFound";
+    this.message = "Este objeto no se encuentra en el array";
+}
+
+notFound.prototype = new myError(); //hereda de myError
+notFound.prototype.constructor = notFound; // creamos un constructor para este tipo de error
+
+// tipo de error personalizado: array lleno
+function fullOf(){ 
+    this.name = "fullOf";
+    this.message = "El array está completo";
+}
+
+fullOf.prototype = new myError(); //hereda de myError
+fullOf.prototype.constructor = fullOf; // creamos un constructor para este tipo de error
+
+// tipo de error personalizado: array lleno
+function emptyOf(){ 
+    this.name = "emptyOf";
+    this.message = "El array está vacío";
+}
+
+emptyOf.prototype = new myError(); //hereda de myError
+emptyOf.prototype.constructor = emptyOf; // creamos un constructor para este tipo de error
+
+
+// tipo de error personalizado: el indice se sale del rango
+function outOfIndex(range){ 
+    this.name = "outOfIndex";
+    this.message = "El indice es incorrecto: debe estar entre el 0 y " + range;
+}
+
+outOfIndex.prototype = new myError(); //hereda de myError
+outOfIndex.prototype.constructor = outOfIndex; // creamos un constructor para este tipo de error
+
+// tipo de error personalizado: el indice introducido es incorrecto
+function incorrectIndex(index){ 
+    this.name = "incorrectIndex";
+    this.message = "El indice " +  index + "   es incorrecto: debe ser un numero";
+}
+
+incorrectIndex.prototype = new myError(); //hereda de myError
+incorrectIndex.prototype.constructor = incorrectIndex; // creamos un constructor para este tipo de error
+
+
+/* fin objeto myError */
 
 
 
 //funciones de testeo
 function testListObj(){
 
-    var per1 = new Person("ivan","cañizares");
-    var per2 = new Person("beatriz","moraleda");
-    var per3 = new Person(); //objeto vacío
-    var per4 = new Person("ruben","martin");
-    var per5 = new Person("maria","caballero");
-    var per6 = new Person1("extra","extra");
+    var per1 = new Person("1 - ivan","cañizares");
+    var per2 = new Person("2 - beatriz","moraleda");
+    var per3 = new Person(); 
+    var per4 = new Person("4 - ruben","martin");
+    var per5 = new Person("5 - maria","caballero");
+    var per6 = new Person1("6 - extra","extra");
    
     var list = new Lista();
+    console.log("Comprobaciones antes de instertar objetos en un array");
+    console.log("¿La lista está vacía? " + list.isEmpty());
+    console.log("¿La lista está llena? " + list.isFull());
     
-    list.addPerson(per1)
-    list.addPerson(per2);
-    console.log(list.addPersonAt(per4,0));
-    list.addPerson(per5);
-    var PerAux = list.getPerson(0);
-    console.log("Persona en la posición 0 del array: " + PerAux.name) ;
-    //console.log(list.clear());
+    console.log("Añadiendo personas al array");
+    console.log("persona añadida; Tamaño actual del array:" + list.addPerson(per1));
+    console.log("persona añadida; Tamaño actual del array:" + list.addPerson(per2));
+    console.log("persona añadida; Tamaño actual del array:" + list.addPerson(per3));
+    console.log("persona añadida; Tamaño actual del array:" + list.addPerson(per4));
+    console.log("persona añadida; Tamaño actual del array:" + list.addPerson(per5));
+   
+    
+    console.log("Comprobaciones despues de instertar objetos en un array");
+    console.log("¿La lista está vacía? " + list.isEmpty());
+    console.log("¿La lista está llena? " + list.isFull());
+    
+    console.log("lista actual:");
+    list.toStringArray();
+    
+    //error al añadir un elemento al array cuando este está lleno
+    console.log("Intento añadir un elemento al array cuando este está lleno");
+    try {
+        console.log(list.addPerson(per5));
+    } catch (error) {
+        console.log(error.toString());
+    }
+    
+    //error al añadir un objeto que no es una instancia de Person
+    console.log("Intento añadir un objeto que no es una instancia de Person");
+    try {
+        console.log(list.addPerson(per6));
+    } catch (error) {
+        console.log(error.toString());
+    }
+
+    var PerAux = list.getPerson(2);
+    console.log("Persona en la posición 2 del array: " + PerAux.name) ;
+    
+    //error al  recoger un objeto que esta fuera del rango del indice
+    console.log("Intento recoger un objeto que esta fuera del rango(9) ");
+    try {
+        console.log(list.getPerson(9));
+    } catch (error) {
+        console.log(error.toString());
+    }
 
     var PerAux = list.firstElement();
     console.log("1º persona del array: " + PerAux.name);
@@ -221,32 +367,80 @@ function testListObj(){
     var PerAux = list.lastElement();
     console.log("última persona del array: " + PerAux.name);
 
+    //error al introducir un caracter como indice al borrar un elemento
+    console.log("Intento introducir un caracter como indice al borrar un elemento (\"letras\")");
+    try {
+        var PerAux = list.remove("letras");
+        console.log("Elemento borrado del array al (el valor del indice es 0 al no pasarle ningun parametro): " + PerAux.name);
+    } catch (error) {
+        console.log(error.toString());
+    }
+
     var PerAux = list.remove();
+    console.log("Elemento borrado del array (el valor del indice es 0 al no pasarle ningun parametro): " + PerAux.name);
+
+    console.log("lista actual:");
+    list.toStringArray();
+
+    var PerAux = list.remove(1);
+    console.log("Elemento borrado del array (1): " + PerAux.name);
+
+    console.log("lista actual:");
+    list.toStringArray();
+
+    //error al intentar borrar un objeto que no se encuentra en el array
+    console.log("Intento borrar un objeto que no está en el array");
+    try {
+        var PerAux = list.removeElement(per1);
+        console.log("Elemento borrado del array (posicion por defecto: 0): " + PerAux.name);
+    } catch (error) {
+        console.log(error.toString());
+    }
+
+    console.log("lista actual:");
+    list.toStringArray();
+
+    var PerAux = list.set(per1);
     console.log("Elemento borrado del array (posicion por defecto: 0): " + PerAux.name);
 
-    var PerAux = list.removeElement(per1);
-    console.log("Elemento borrado del array (posicion por defecto: 0): " + PerAux.name);
+    console.log("lista actual:");
+    list.toStringArray();
 
-    /*
+    //error al intentar introducir un objeto fuera de rango
+    console.log("Intento introducir un objeto fuera de rango (10)");
     try {
-        list.addPerson(per6);//este debe lanzar la excepción: no es una instancia de "Persona" ya que es un objeto "Persona1")
+        list.addPersonAt(per1,10); //debe lanzar la excepción: fuera de rango.
     } catch (error) {
-        console.log(error);
+        console.log(error.toString());
     }
-    
-    try {
-        list.addPerson(per5); //debe lanzar la excepción: la lista esta completa.
-    } catch (error) {
-        console.log(error);
-    }
-    */
+
+    console.log("lista actual:");
+    list.toStringArray();
+
+    console.log("añado per2 en la posicion 0");
+    list.addPersonAt(per2,0); 
+
+    console.log("lista actual:");
+    list.toStringArray();
 
     console.log("El tamaño actual de la lista es:" + list.listSize()); 
 
     console.log("¿La lista está vacía? " + list.isEmpty());
     console.log("¿La lista está llena? " + list.isFull());
 
+    console.log("vacío la lista")
+    console.log(list.clear());
+
+    console.log("lista actual:");
     list.toStringArray();
-	
+
+    //error al intentar borrar un objeto cuando el array está vacío
+    console.log("Intento borrar un objeto cuando el array está vacío");
+    try {
+        var PerAux = list.remove(0);
+        } catch (error) {
+        console.log(error.toString());
+    }
+
 }
 window.onload = (testListObj);
